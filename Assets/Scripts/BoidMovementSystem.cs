fa01003e2925a60b989a59b5008c8e58723baaee
pstrimar <pstrimar@gmail.com>
2021-02-24 17:01:48 -0500

    Initial commit

---
+++ Assets/Scripts/BoidMovementSystem.cs
@@ -0,0 +1,135 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class BoidMovementSystem : JobComponentSystem
+{
+    private ECSManager controller;
+
+    private struct EntityWithLocalToWorld
+    {
+        public Entity entity;
+        public LocalToWorld localToWorld;
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        if (!controller)
+            controller = ECSManager.Instance;
+
+        if (controller)
+        {
+            EntityQuery boidQuery = GetEntityQuery(ComponentType.ReadOnly<BoidData>(), ComponentType.ReadOnly<LocalToWorld>());
+
+            NativeArray<Entity> entityArray = boidQuery.ToEntityArray(Allocator.TempJob);
+            NativeArray<LocalToWorld> localToWorldArray = boidQuery.ToComponentDataArray<LocalToWorld>(Allocator.TempJob);
+
+            NativeArray<EntityWithLocalToWorld> boidArray = new NativeArray<EntityWithLocalToWorld>(entityArray.Length, Allocator.TempJob);
+            NativeArray<float4x4> newBoidTransforms = new NativeArray<float4x4>(entityArray.Length, Allocator.TempJob);
+
+            for (int i = 0; i < entityArray.Length; i++)
+            {
+                boidArray[i] = new EntityWithLocalToWorld
+                {
+                    entity = entityArray[i],
+                    localToWorld = localToWorldArray[i]
+                };
+            }
+
+            entityArray.Dispose();
+            localToWorldArray.Dispose();
+
+            float cageLimits = controller.cageLimits;
+            float boidSpeed = controller.boidSpeed;
+            float boidPerceptionRadius = controller.boidPerceptionRadius;
+            float separationWeight = controller.separationWeight;
+            float cohesionWeight = controller.cohesionWeight;
+            float alignmentWeight = controller.alignmentWeight;
+            float avoidWallsWeight = controller.avoidWallsWeight;
+            float avoidWallsTurnDist = controller.avoidWallsTurnDist;
+
+            float deltaTime = Time.DeltaTime;
+
+            JobHandle boidJobHandle = Entities
+                .WithAll<BoidData>()
+                .WithBurst()
+                .WithReadOnly(boidArray)
+                .WithDisposeOnCompletion(boidArray)
+                .ForEach((Entity boid, int entityInQueryIndex, ref LocalToWorld localToWorld) =>
+                {
+                    float3 boidPosition = localToWorld.Position;
+                    float3 separationSum = float3.zero;
+                    float3 positionSum = float3.zero;
+                    float3 headingSum = float3.zero;
+
+                    int boidsNearby = 0;
+
+                    for (int otherBoidIndex = 0; otherBoidIndex < boidArray.Length; otherBoidIndex++)
+                    {
+                        if (boid != boidArray[otherBoidIndex].entity)
+                        {
+                            float3 otherPosition = boidArray[otherBoidIndex].localToWorld.Position;
+                            float distToOtherBoid = math.length(boidPosition - otherPosition);
+
+                            if (distToOtherBoid < boidPerceptionRadius)
+                            {
+                                // avoid dividing by zero
+                                separationSum += (boidPosition - otherPosition) * (1f / math.max(distToOtherBoid, .0001f));
+                                positionSum += otherPosition;
+                                headingSum += boidArray[otherBoidIndex].localToWorld.Forward;
+
+                                boidsNearby++;
+                            }
+                        }
+                    }
+
+                    float3 force = float3.zero;
+
+                    if (boidsNearby > 0)
+                    {
+                        // move away from average of neighbors position
+                        force += (separationSum / boidsNearby) * separationWeight;
+                        // move towards center of group
+                        force += ((positionSum / boidsNearby) - boidPosition) * cohesionWeight;
+                        // move towards average heading of group
+                        force += (headingSum / boidsNearby) * alignmentWeight;
+                    }
+                    if (math.min(math.min(
+                        (cageLimits / 2f) - math.abs(boidPosition.x),
+                        (cageLimits / 2f) - math.abs(boidPosition.y)),
+                        (cageLimits / 2f) - math.abs(boidPosition.z))
+                        < avoidWallsTurnDist)
+                    {
+                        // move towards center of bounds if too close to the limit
+                        force += -math.normalize(boidPosition) * avoidWallsWeight;
+                    }
+
+                    float3 velocity = localToWorld.Forward * boidSpeed;
+                    velocity += force * deltaTime;
+                    velocity = math.normalize(velocity) * boidSpeed;
+
+                    newBoidTransforms[entityInQueryIndex] = float4x4.TRS(
+                        localToWorld.Position + velocity * deltaTime,
+                        quaternion.LookRotationSafe(velocity, localToWorld.Up),
+                        new float3(1f, 1f, 1f)
+                        );
+                }).Schedule(inputDeps);
+
+            JobHandle boidMoveJob = Entities
+                .WithAll<BoidData>()
+                .WithBurst()
+                .WithReadOnly(newBoidTransforms)
+                .WithDisposeOnCompletion(newBoidTransforms)
+                .ForEach((Entity boid, int entityInQueryIndex, ref LocalToWorld localToWorld) =>
+                {
+                    localToWorld.Value = newBoidTransforms[entityInQueryIndex];
+                }).Schedule(boidJobHandle);
+
+            return boidMoveJob;
+        }
+        else
+            return inputDeps;
+    }
+}
---
+++ Assets/Scripts/ECSManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Collections;
+
+public class ECSManager : MonoBehaviour
+{
+    public static ECSManager Instance;
+
+    public float cageLimits;
+    public float boidSpeed;
+    public float boidPerceptionRadius;
+    public float separationWeight;
+    public float cohesionWeight;
+    public float alignmentWeight;
+    public float avoidWallsWeight;
+    public float avoidWallsTurnDist;
+
+    [SerializeField]
+    int numBoids = 200;
+
+    [SerializeField]
+    Mesh sharedMesh;
+
+    [SerializeField]
+    Material sharedMaterial;
+
+    EntityManager entityManager;
+
+    void Awake()
+    {
+        Instance = this;
+
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype boidArchetype = entityManager.CreateArchetype(
+            typeof(BoidData),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            );
+
+        NativeArray<Entity> boidArray = new NativeArray<Entity>(numBoids, Allocator.Temp);
+        entityManager.CreateEntity(boidArchetype, boidArray);
+
+        for (int i = 0; i < boidArray.Length; i++)
+        {
+            Unity.Mathematics.Random rand = new Unity.Mathematics.Random((uint)i + 1);
+
+            entityManager.SetComponentData(boidArray[i], new LocalToWorld
+            {
+                Value = float4x4.TRS(
+                    RandomPosition(),
+                    RandomRotation(),
+                    new float3(1f, 1f, 1f))
+            });
+
+            entityManager.SetSharedComponentData(boidArray[i], new RenderMesh
+            {
+                mesh = sharedMesh,
+                material = sharedMaterial
+            });            
+        }
+        boidArray.Dispose();
+    }
+
+    private float3 RandomPosition()
+    {
+        return new float3(
+            UnityEngine.Random.Range(-cageLimits / 2f, cageLimits / 2f),
+            UnityEngine.Random.Range(-cageLimits / 2f, cageLimits / 2f),
+            UnityEngine.Random.Range(-cageLimits / 2f, cageLimits / 2f));
+    }
+
+    private quaternion RandomRotation()
+    {
+        return quaternion.Euler(
+            UnityEngine.Random.Range(-360f, 360f),
+            UnityEngine.Random.Range(-360f, 360f),
+            UnityEngine.Random.Range(-360f, 360f));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(cageLimits, cageLimits, cageLimits));
+    }
+}
